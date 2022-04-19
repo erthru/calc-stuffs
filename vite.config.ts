dae43465e5cad3b35cd7a25c9c44c5376eb55985
Anto D <ersaka96@gmail.com>
2022-04-19 19:57:36 +0800

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 import Components from "unplugin-vue-components/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,39 @@ export default defineConfig({
             directoryAsNamespace: true,
             globalNamespaces: ["common"],
             dts: true,
+        }),
+
+        VitePWA({
+            includeAssets: [
+                "favicon.svg",
+                "favicon.ico",
+                "robots.txt",
+                "apple-touch-icon.png",
+            ],
+            manifest: {
+                name: "Calc Stuffs - Calculate Your Stuffs",
+                short_name: "Calc Stuffs",
+                description: "Calculate Your Stuffs",
+                theme_color: "#ef4444d9",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
+            },
         }),
     ],
 
